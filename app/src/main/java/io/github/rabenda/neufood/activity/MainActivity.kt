f469be4b98240f66a38cee5051524d2d3b8be124
Rabenda <rabenda.cn@gmail.com>
2018-10-09 16:30:01 +0800

    Add mainActivity bottom

--- app/src/main/java/io/github/rabenda/neufood/activity/MainActivity.kt
+++ app/src/main/java/io/github/rabenda/neufood/activity/MainActivity.kt
@@ -1,25 +1,27 @@
 package io.github.rabenda.neufood.activity
 
+import android.app.Fragment
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import io.github.rabenda.neufood.R
+import io.github.rabenda.neufood.fragment.StoreFragment
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    val storeFragment = StoreFragment()
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.neufood_store -> {
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -30,6 +32,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
