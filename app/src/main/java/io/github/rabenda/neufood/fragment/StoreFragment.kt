ea677bed3ba0f19d3f0c59521f87bc34d4f3f912
Rabenda <rabenda.cn@gmail.com>
2018-11-28 16:59:41 +0800

    Update dependencies

--- app/src/main/java/io/github/rabenda/neufood/fragment/StoreFragment.kt
+++ app/src/main/java/io/github/rabenda/neufood/fragment/StoreFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_login.*
 
 class StoreFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
